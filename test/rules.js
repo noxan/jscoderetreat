233dbc540f89b44a2fe2ccd7bebebe9e9f5affbf
Richard <hello@noxan.net>
2015-12-05 12:02:17 +0100

    add test case with object of neighbors and according to their position

--- test/rules.js
+++ test/rules.js
@@ -2,9 +2,13 @@ var expect = require('unexpected');
 
 var gol = require('../gol');
 
+
 describe('gameoflife', () => {
   describe('number of neighbors', () => {
-    expect(gol.neighborCount(5, 2), 'to be', 1);
+    expect(gol.neighborCount({
+      n: true,
+      w: true,
+    }), 'to be', 2);
   });
   describe('single neighbor', () => {
     it('should die', () => {
