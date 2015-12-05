a6ccd537fed6d315f9a8c155b77516eb71ed3cd3
Richard <hello@noxan.net>
2015-12-05 11:39:04 +0100

    write test case for gol

--- test/rules.js
+++ test/rules.js
@@ -1,1 +1,14 @@
+var expect = require('unexpected');
+
+var gol = require('../gol');
+
+
+describe('gameoflife', function () {
+  describe('single neighbor', function () {
+    it('should die', function () {
+      expect(gol.alive(5, 2), 'to be falsy');
+    });
+  });
+});
+
 module.exports = {};
