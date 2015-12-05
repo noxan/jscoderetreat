c98ba6d5ebcd368c39b6d73f64c036c1f669a1bb
Richard <hello@noxan.net>
2015-12-05 11:46:37 +0100

    use fancy arrow functions

--- test/rules.js
+++ test/rules.js
@@ -2,9 +2,9 @@ var expect = require('unexpected');
 
 var gol = require('../gol');
 
-describe('gameoflife', function () {
-  describe('single neighbor', function () {
-    it('should die', function () {
+describe('gameoflife', () => {
+  describe('single neighbor', () => {
+    it('should die', () => {
       expect(gol.alive(5, 2), 'to be falsy');
     });
   });
