1fcc6a878b310a4492a3d9e4fedd9f1861415adb
Richard <hello@noxan.net>
2015-12-05 11:48:31 +0100

    expect number of neighbors for specific position to be 1

--- test/rules.js
+++ test/rules.js
@@ -3,6 +3,9 @@ var expect = require('unexpected');
 var gol = require('../gol');
 
 describe('gameoflife', () => {
+  describe('number of neighbors', () => {
+    except(gol.neighborCount(5, 2), 'to be', 1);
+  });
   describe('single neighbor', () => {
     it('should die', () => {
       expect(gol.alive(5, 2), 'to be falsy');
